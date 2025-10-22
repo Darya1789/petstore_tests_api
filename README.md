84a0e09bee7e66f5e9abeaa08e9d418b2490fd98
Darya Matrosova <mostovichdi@gmail.com>
2025-10-22 22:56:10 +0300

    Create allure

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # petstore_tests_api
 API tests for petstore
+
+## Структура проекта
+petstore_tests_api/
+├── conftest.py # Фикстуры Pytest
+├── requirements.txt # Зависимости проекта
+├── README.md # Документация
+│── helper.py # Генераторы данных
+│── data.py # Тестовые данные
+│── urls.py # Урлы 
+│
+├── tests/ # Тестовые сценарии
+│ ├── test_pet_api.py # Тесты для эндпоинтов Pet
+│ ├── test_store_api.py # Тесты для эндпоинтов Store
+│ └── test_user_api.py # Тесты для эндпоинтов User
+│
+├── methods/ # API методы и клиенты
+│ ├── pet_api.py # Методы для работы с Pet API
+│ ├── store_api.py # Методы для работы с Store API
+│ └── user_api.py # Методы для работы с User API
+
+## Allure отчет
+Для просмотра отчета выполнить команду в директории проекта
+> allure serve allure-results
+
+## Зависимости
+Ддя уставновки зависимостей ввести команду
+> pip install -r requirements.txt
--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,55 +1,97 @@
 import pytest
+import allure
 from methods.pet_api import PetApi
 from data import ExpectedData, TestData
 
-
+@allure.suite('API тесты для pet')
 class TestPet:
-    def test_add_new_pet(self, create_pet_data):
-        pet = create_pet_data
-        api = PetApi()
-        response = api.add_new_pet(pet)
-        assert response.status_code == 200
-        response_data = response.json()
-        assert response_data['name'] == create_pet_data['name']
-        assert response_data['photoUrls'] == create_pet_data['photoUrls']
-        assert response_data['status'] == create_pet_data['status']
 
+    @allure.title('Проверка создания питомца с обязательными параметрами')
+    def test_add_new_pet(self, create_pet_data):
+        with allure.step('Генерируем данные для создания питомца'):
+            pet = create_pet_data
+        api = PetApi()
+        with allure.step('Отправляем запрос на создание питомца'):
+            response = api.add_new_pet(pet)
+        with allure.step('Проверяем код ответа'):
+            assert response.status_code == 200
+        with allure.step('Сохраняем данные ответа'):
+            response_data = response.json()
+        with allure.step('Проверяем имя созданного питомца'):
+            assert response_data['name'] == create_pet_data['name']
+        with allure.step('Проверяем url фото созданного питомца'):
+            assert response_data['photoUrls'] == create_pet_data['photoUrls']
+        with allure.step('Проверяем статус сохданного питомца'):
+            assert response_data['status'] == create_pet_data['status']
+    
+    
+    @allure.title('Проверка создания питомца без параметра {param}')
     @pytest.mark.parametrize('param', ['name', 'photoUrls'])
     def test_add_new_pet_without_required_param(self, create_pet_data, param):
-        pet = create_pet_data
-        pet[f'{param}'] = ''
+        with allure.step('Генерируем данные для создания питомца'):
+            pet = create_pet_data
+        with allure.step(f'Обнуляем параметр {param}'):
+            pet[f'{param}'] = ''
         api = PetApi()
-        response = api.add_new_pet(pet)
-        assert response.status_code == 405
-        response_data = response.json()
-        assert response_data['message'] == ExpectedData.ADD_PET_INCORRECT_INPUT
+        with allure.step('Отправляем запрос на создание питомца'):
+            response = api.add_new_pet(pet)
+        with allure.step('Проверяем код ответа'):
+            assert response.status_code == 405
+        with allure.step('Сохраняем данные ответа'):
+            response_data = response.json()
+        with allure.step('Проверяем сообщение об ошибке'):
+            assert response_data['message'] == ExpectedData.ADD_PET_INCORRECT_INPUT
 
+    @allure.title('Проверка обновления данных питомца')
     def test_update_pet(self, add_pet_to_store):
-        new_pet = add_pet_to_store.json()
-        new_pet['name'] = TestData.new_name_for_update_pet
+        with allure.step('Создаем питомца'):
+            new_pet = add_pet_to_store.json()
+        with allure.step('Обновляем имя питомца'):
+            new_pet['name'] = TestData.new_name_for_update_pet
         api = PetApi()
-        response = api.update_pet(new_pet)
-        assert response.status_code == 200
-        response_data = response.json()
-        assert response_data['name'] == TestData.new_name_for_update_pet
+        with allure.step('Отправляем запрос на обновление питомца'):
+            response = api.update_pet(new_pet)
+        with allure.step('Проверяем код ответа'):
+            assert response.status_code == 200
+        with allure.step('Сохраняем данные ответа'):
+            response_data = response.json()
+        with allure.step('Проверяем обновленное имя питомца'):
+            assert response_data['name'] == TestData.new_name_for_update_pet
     
+    @allure.title('Проверка получения информации о питомце по id')
     def test_get_pet_by_id(self, add_pet_to_store):
-        pet_id = add_pet_to_store.json()['id']
+        with allure.step('Создаем питомца и получаем id'):
+            pet_id = add_pet_to_store.json()['id']
         api = PetApi()
-        response = api.get_pet_by_id(pet_id)
-        assert response.status_code == 200
-        response_data = response.json()
-        assert response_data['name'] == add_pet_to_store.json()['name']
-        assert response_data['photoUrls'] == add_pet_to_store.json()['photoUrls']
-        assert response_data['status'] == add_pet_to_store.json()['status']
+        with allure.step('Отправляем запрос на получение информации о питомце'):
+            response = api.get_pet_by_id(pet_id)
+        with allure.step('Проверяем код ответа'):
+            assert response.status_code == 200
+        with allure.step('Сохраняем данные ответа'):
+            response_data = response.json()
+        with allure.step('Проверяем имя питомца'):
+            assert response_data['name'] == add_pet_to_store.json()['name']
+        with allure.step('Проверяем url фото питомца'):
+            assert response_data['photoUrls'] == add_pet_to_store.json()['photoUrls']
+        with allure.step('Проверяем статус питомца'):
+            assert response_data['status'] == add_pet_to_store.json()['status']
 
+    
+    @allure.title('Проверка удаления питомца')
     def test_delete_pet(self, create_pet_data):
-        pet = create_pet_data
+        with allure.step('Генерируем данные для создания питомца'):
+            pet = create_pet_data
         api = PetApi()
-        response_add = api.add_new_pet(pet)
-        pet_id = response_add.json()['id']
-        response = api.delete_pet(pet_id)
-        assert response.status_code == 200
-        response_get = api.get_pet_by_id(pet_id)
-        assert response_get.status_code == 404        
+        with allure.step('Отправляем запрос на создание питомца'):
+            response_add = api.add_new_pet(pet)
+        with allure.step('Получаем id созданного питомца'):
+            pet_id = response_add.json()['id']
+        with allure.step('Отправляем запрос на удаление питомца'):
+            response = api.delete_pet(pet_id)
+        with allure.step('Проверяем код ответа'):
+            assert response.status_code == 200
+        with allure.step('Отправляем запрос на получение информации об удаленном питомце'):
+            response_get = api.get_pet_by_id(pet_id)
+        with allure.step('Проверяем код ошибки'):
+            assert response_get.status_code == 404        
         
